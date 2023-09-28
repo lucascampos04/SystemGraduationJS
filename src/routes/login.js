b0fcac4eaee15eeb25de837098511208e8dd9dba
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-28 16:43:50 -0300

    Verificação de login funcionando

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,22 +1,38 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const rotaHome = require("./home")
+const conn = require("../models/database/connect")
 const rotas = express.Router();
-
 const usuariosAutenticados = require("../models/users");
 
+rotas.use(bodyParser.urlencoded({extended:true}))
+
 rotas.post("/login", (req, res) => {
-   const { username, senha } = req.body;
+  const { username, senha } = req.body;
 
   console.log("username:", username);
   console.log("senha:", senha);
 
-  if (username === usuariosAutenticados.username && senha === usuariosAutenticados.senha) {
-    // res.json(usuariosAutenticados);
-    res.redirect("/")
-  } else {
-    console.log("Autenticação falhou");
-    res.status(401).json({ error_login: "Credenciais inválidas" });
-  }
+  const query = 'SELECT matricula, usuario FROM logsfuncionarios WHERE usuario = ? AND senha = ?';
+  conn.query(query, [username, senha], (error, results) => {
+    if (error) {
+      console.error('Erro ao executar a consulta:', error);
+      conn.end(); 
+      const error_login = "Erro ao verificar o login.";
+      res.render("login", { error_login });
+    } else if (results.length > 0) {
+      const { matricula, usuario } = results[0];
+      console.log(`Login bem-sucedido. Usuário: ${usuario} (Matrícula: ${matricula})`);
+      conn.end(); 
+      res.redirect("/");
+    } else {
+      
+      console.log("Autenticação falhou");
+      conn.end(); 
+      const error_login = "Credenciais inválidas";
+      res.render("login", { error_login });
+    }
+  });
 });
 
 rotas.get("/login", (req, res) => {
